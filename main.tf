ade82e12c170ace4a3b392607aa927315fe4d005
Alex Rodrigues da Silva <alerodrigues@uolinc.com>
2020-12-14 16:46:09 -0300

    adicionado criação de security_group e atachando nas EC2 criadas

--- main.tf
+++ main.tf
@@ -10,8 +10,25 @@ resource "aws_instance" "dev" {
   key_name = "terraform-aws"
   tags = {
       Name = "dev${count.index}"
-  } 
+  }
+  vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
   subnet_id = "subnet-0b38d89493d213e5a"
 }
 
+resource "aws_security_group" "acesso-ssh" {
+  name = "acesso-ssh"
+  description = "acesso-ssh"
+  vpc_id      = "vpc-0bf7ff9f7c61d6803"
+  
+  ingress {
+  from_port = 22
+  to_port = 22  
+  protocol =  "tcp"
+  
+  cidr_blocks = ["200.221.157.12/32"]
+  }
 
+  tags = {
+    Name = "ssh"
+  }
+}
